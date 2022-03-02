2812257de6005251cb4773f04172c8d50b4982e6
widyasw <99782577+widyasw@users.noreply.github.com>
2022-03-02 08:27:54 +0700

    Update 2.4.cpp

--- 2.4.cpp
+++ 2.4.cpp
@@ -1,56 +1,46 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-  int matriks1[10][10], matriks2[10][10], hasil[10][10];
-  int i, j, k, m, n, p, q, jumlah = 0;
-  cout << "Masukkan jumlah baris matriks pertama: ";
-  cin >> m;
-  cout << "Masukkan jumlah kolom matriks pertama: ";
-  cin >> n;
-
-  cout << "Masukkan jumlah baris matriks kedua: ";
-  cin >> p;
-  cout << "Masukkan jumlah kolom matriks kedua: ";
-  cin >> q;
-
-  if(n != p){
-    cout << "Matriks tidak dapat dikalikan.\n";
-  } else {
-
-    cout << "Masukkan elemen matriks pertama: \n";
-    for(i = 0; i < m; i++){
-      for(j = 0; j < n; j++){
-        cin >> matriks1[i][j];
-      }
-    }
-
-    cout << "Masukkan elemen matriks kedua: \n";
-    for(i = 0; i < p; i++){
-      for(j = 0; j < q; j++){
-        cin >> matriks2[i][j];
-      }
-    }
-
-    for(i = 0; i < m; i++){
-      for(j = 0; j < q; j++){
-        for(k = 0; k < p; k++){
-          jumlah = jumlah + matriks1[i][k] * matriks2[k][j];
-        }
-        hasil[i][j] = jumlah;
-        jumlah = 0;
-      }
-    }
-
-    cout << "Hasil perkalian matriks: \n";
-    for(i = 0; i < m; i++){
-      for(j = 0; j < q; j++){
-        cout << hasil[i][j] << "\t";
-      }
-      cout << endl;
-    }
-
-  }
-  
-  return 0;
+int main()
+{
+	int matA[2][2] = {4,2,8,10};
+	int matB[2][2] = {-5,4,8,-12};
+	int hasil[2][2];
+	int i,j,k, jumlah = 0;
+	
+	cout << " Matriks A " << endl;
+	for (int i=0; i<2; i++){
+		for (int j=0; j<2; j++){
+			cout << matA[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << " Matriks B " << endl;
+	for (int i=0; i<2; i++){
+		for (int j=0; j<2; j++){
+			cout << matB[i][j] << " ";
+		}
+		cout << endl;
+	}
+	for (int i=0; i<2; i++){
+		for (int j=0; j<2; j++){
+			for (int k=0; k<2; k++){
+				jumlah = jumlah + matA[i][k] * matB[k][j];
+			}
+			hasil[i][j] = jumlah;
+			jumlah = 0;
+		}
+		cout << endl;
+	}
+	
+	cout << " Hasil Perkalian Matriks A dan B " << endl;
+	for (int i=0; i<2; i++){
+		for (int j=0; j<2; j++){
+			cout << hasil[i][j] << " ";
+		}
+		cout << endl;
+	}
+	
+	system (" PAUSE ");
+	return 0;
 }
